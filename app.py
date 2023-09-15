57e37f834faee1dcf8ed220157a6afa8744ae88c
Renda Zhang <renda@michaels.com>
2023-09-15 16:36:07 +0800

    Enable testing

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify, render_template, stream_with_context, Response
 from dashscope import Generation
+import time
 import json
 
 app = Flask(__name__)
 
-@app.route('/chat', methods=['POST'])
+#@app.route('/chat', methods=['POST'])
 def chat():
     content = request.json
     user_message = content.get('message')
@@ -27,6 +28,31 @@ def generate_response(prompt_text):
             new_message = current_message[last_length:]
             last_length = len(current_message)
             yield json.dumps({"text": new_message}).encode('utf-8') + b'\n'
+
+# Simulated Generator (for testing purposes)
+def simulated_generator():
+    messages = [
+        "I am a",
+        "I am a large language model created by DAMO Academy",
+        ". I am called QianWen",
+        "."
+    ]
+    
+    for message in messages:
+        yield message
+        time.sleep(1)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_message = request.json.get('message')
+
+    # This is just a simulated response. In reality, you'd call your model here.
+    def generate_response():
+        for chunk in simulated_generator():
+            yield json.dumps({"text": chunk}).encode('utf-8') + b'\n'
+
+    return Response(stream_with_context(generate_response()), content_type='application/json')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
