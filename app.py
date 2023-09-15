021efa00cd4b0b93c9b295c83375533ff42e4f30
Renda Zhang <renda@michaels.com>
2023-09-15 15:00:57 +0800

    Change again for the streaming output function

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, stream_with_context
 from dashscope import Generation
 
 app = Flask(__name__)
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    data = request.get_json()
-    user_message = data['message']
-    
-    # Use the dashscope SDK to get the response
+    content = request.json
+    user_message = content.get('message')
+
+    response_generator = generate_response(user_message)
+
+    return Response(stream_with_context(response_generator), content_type='text/plain; charset=utf-8')
+
+def generate_response(prompt_text):
     response_generator = Generation.call(
         model='qwen-turbo',
-        prompt=user_message,
+        prompt=prompt_text,
         stream=True,
         top_p=0.8
     )
-    
-    def generate():
-        head_idx = 0
-        for resp in response_generator:
-            paragraph = resp.output['text']
-            # Extract the new part of the text to send
-            new_text = paragraph[head_idx:len(paragraph)]
-            if paragraph.rfind('\n') != -1:
-                head_idx = paragraph.rfind('\n') + 1
-            # Yield the new part of the text as a JSON string
-            yield f"data: {json.dumps({'message': new_text})}\n\n"
-    
-    return Response(generate(), mimetype='text/event-stream')
+    full_text = ""
+    for response in response_generator:
+        if "text" in response.output:
+            # Extend the text with the new chunk and yield it
+            full_text += response.output["text"]
+            yield full_text
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, threaded=True)
+    app.run(debug=True, port=8080)
