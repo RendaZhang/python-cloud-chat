1fcfbf5d249aaf4ddc2d624f61145b2afa965774
Renda Zhang <renda@michaels.com>
2023-09-15 11:41:26 +0800

    Fix streamming bug

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
-from flask import Flask, request, stream_with_context, Response
-from http import HTTPStatus
+from flask import Flask, request, jsonify, Response
 from dashscope import Generation
 
 app = Flask(__name__)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    message = data['message']
+
+    response_generator = Generation.call(
+        model='qwen-turbo',
+        prompt=message,
+        stream=True,
+        top_p=0.8
+    )
+
+    def generate():
+        head_idx = 0
+        for resp in response_generator:
+            paragraph = resp.output['text']
+            yield paragraph[head_idx:]
+            if paragraph.rfind('\n') != -1:
+                head_idx = paragraph.rfind('\n') + 1
+
+    return Response(generate(), content_type='text/plain')
 
 @app.route('/')
 def hello():
     return "Hello from Flask!"
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.get_json()
-    message = data.get('message', '')
-
-    # The AI response will be streamed using the generator function
-    def generate():
-        prompt_text = message
-        response_generator = Generation.call(
-            model='qwen-turbo',
-            prompt=prompt_text,
-            stream=True,
-            top_p=0.8)
-
-        head_idx = 0
-        for resp in response_generator:
-            paragraph = resp.output['text']
-            yield paragraph[head_idx:len(paragraph)]
-            if paragraph.rfind('\n') != -1:
-                head_idx = paragraph.rfind('\n') + 1
-
-    return Response(stream_with_context(generate()), content_type='text/plain')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
