87b8910981a9ba75fbf986c9403d9298e6900d17
Renda Zhang <renda@michaels.com>
2023-11-29 09:50:15 +0800

    Update gpt_chat route

--- app.py
+++ app.py
@@ -64,15 +64,17 @@ def gpt_chat():
 
     return Response(stream_with_context(response_json), content_type='application/json')
 
-def gpt_generate_response(prompt_text):
+def gpt_generate_response(user_input):
     try:
-        response = openai.Completion.create(
-            engine="gpt-4",  # Using GPT-4 model
-            prompt=prompt_text,
-            max_tokens=150  # Adjust the number of tokens as needed
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # Adjust model as needed
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_input}
+            ]
         )
-        text = response.choices[0].text.strip()
-        yield json.dumps({"text": text}).encode('utf-8') + b'\n'
+        assistant_reply = response['choices'][0]['message']['content']
+        yield json.dumps({"text": assistant_reply}).encode('utf-8') + b'\n'
     except Exception as e:
         yield json.dumps({"error": str(e)}).encode('utf-8') + b'\n'
 
